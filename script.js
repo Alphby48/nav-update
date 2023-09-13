61332894ecd6704ede3fc211ec3de866c9bb2d38
Alphby48 <hazerdmobile@gmail.com>
2023-09-13 22:37:50 +0700

    1

---
+++ script.js
@@ -0,0 +1,26 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("slide");
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("slide");
+  }
+});
+
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
