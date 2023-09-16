ec7f13e7140690b19cb619351f15c8250f1ffb7d
Alphby48 <hazerdmobile@gmail.com>
2023-09-17 02:09:08 +0700

    third

--- script.js
+++ script.js
@@ -13,20 +13,62 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("slide");
   }
 });
-
+//
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 const tlsn = document.querySelectorAll(".font-mode");
+const main = document.querySelector("main");
 function switchTheme(e) {
   if (e.target.checked) {
     for (var i = 0; i < tlsn.length; i++) {
       tlsn[i].style.color = "white";
     }
+    main.style.backgroundColor = "#161625";
     document.documentElement.setAttribute("data-theme", "dark");
   } else {
     for (var i = 0; i < tlsn.length; i++) {
       tlsn[i].style.color = "black";
     }
+    main.style.backgroundColor = "#302ae662";
     document.documentElement.setAttribute("data-theme", "light");
   }
 }
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+//
+
+const type = document.querySelector(".type-text");
+
+let listTexts = ["Web Developer", "FE Developer", "Freelancer"];
+
+let index = -1;
+let current = 0;
+
+function typeTextFunc() {
+  if (current === listTexts.length) {
+    current = 0;
+  }
+  let typeText = setInterval(() => {
+    if (index == listTexts[current].length - 1) {
+      clearInterval(typeText);
+      clearTextFunc();
+    } else {
+      index++;
+      type.textContent += listTexts[current][index];
+    }
+  }, 150);
+}
+
+function clearTextFunc() {
+  let clearText = setInterval(() => {
+    if (type.textContent.length == 0) {
+      clearInterval(clearText);
+      index = -1;
+      current++;
+      typeTextFunc();
+    } else {
+      type.textContent = type.textContent.slice(0, -1);
+    }
+  }, 150);
+}
+
+typeTextFunc();
