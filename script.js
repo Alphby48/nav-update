f5c41cf2eced7342b76ae4a4f98c34e1c7969d6a
Alphby48 <hazerdmobile@gmail.com>
2023-10-15 14:15:07 +0700

    v.beta

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const hamburger = document.querySelector("#hamburger-menu");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("slide");
+    hamburger.checked = false;
   }
 });
 //
